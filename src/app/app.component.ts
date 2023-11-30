b43ecd050d5286b62b78800a0d66def8cbd8eecd
Ssreen <sudamallasreenivasula9@gmail.com>
2023-11-30 20:38:03 +0530

    seven

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import { FetchDataService } from './fetch-data.service';
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Demo';
+  userList:any[] = [];
+  constructor(private fetchService: FetchDataService) {}
+  ngOnInit(): void{
+    this.fetchService.getUser().subscribe(user => this.userList = user);
+  }
+}
